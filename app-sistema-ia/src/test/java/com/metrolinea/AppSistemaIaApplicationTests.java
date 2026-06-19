04293b52d40387b2e02da36e12d60321d28632db
Davidortiz28 <davidvega2955@gmail.com>
2026-06-19 15:35:37 -0500

    fix: :fire: Idk
    Idk
    
    Idk

---
+++ app-sistema-ia/src/test/java/com/metrolinea/AppSistemaIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metrolinea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSistemaIaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
