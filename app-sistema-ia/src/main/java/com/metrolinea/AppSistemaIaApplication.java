60c73e71d2a08c7b0090c3f714b2c90e2c5e0db2
Manuel Isaac Camaño Diaz <247783877+manuelisaaccamanidiaz-lgtm@users.noreply.github.com>
2026-06-19 13:11:00 -0500

    Primer commit: Estructura base de Spring Boot

---
+++ app-sistema-ia/src/main/java/com/metrolinea/AppSistemaIaApplication.java
@@ -0,0 +1,13 @@
+package com.metrolinea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSistemaIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSistemaIaApplication.class, args);
+	}
+
+}
